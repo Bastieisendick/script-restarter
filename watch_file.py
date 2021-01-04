1537bb8c854f055a930efdf19083cb2e638d6235
Bastieisendick <39188927+Bastieisendick@users.noreply.github.com>
2021-01-04 22:02:47 +0100

    Add files via upload

---
+++ watch_file.py
@@ -0,0 +1,33 @@
+import time
+import subprocess
+import os
+
+
+os.chdir("/home/pi/heinz")
+
+from threading import Thread
+def watch_thread():
+    try:
+        global file_watcher
+        while True:
+            if(file_watcher.is_alive()==False):
+                file_watcher = Thread(target=watch_file, args=())
+                file_watcher.start()
+                print("started file_watcher")
+            time.sleep(5)
+    except:
+        watch_thread()
+        
+
+def watch_file():
+    while True:
+        subprocess.call(" python3 speech.py", shell=True)
+        print("process disturbed")
+        time.sleep(5)
+        
+
+global file_watcher
+print("working")
+file_watcher = Thread(target=watch_file, args=())
+file_watcher.start()
+watch_thread()
